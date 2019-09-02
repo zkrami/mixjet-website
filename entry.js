bef4dd9f1c97837d2c234668edc4c37a40d29e58
Rami Al-Zebak <17zrami@gmail.com>
2019-09-02 18:11:14 +0300

    generating random clouds

--- entry.js
+++ entry.js
@@ -5,130 +5,6 @@ import { TweenMax, TimelineMax, Power3, Sine, Bounce, Elastic, Power2, Power0 } 
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
-function _clouds() {
-    let cloudsTimeLine1 = new TimelineMax({ repeat: -1 });
-
-    cloudsTimeLine1.to($(".cloud-size-1 image"), 1, {
-        bezier: {
-            values: [{ y: "+=5" }, { y: "-=15" }, { y: "+=15" }, { y: "-=5" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-    cloudsTimeLine1.set($(".cloud-size-1 image"), {
-        x: "+=3200",
-    });
-    cloudsTimeLine1.to($(".cloud-size-1 image"), 1, {
-        bezier: {
-            values: [{ y: "-=5" }, { y: "+=15" }, { y: "-=15" }, { y: "+=5" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-
-
-    let cloudsTimeLine2 = new TimelineMax({ repeat: -1 });
-
-    cloudsTimeLine2.to($(".cloud-size-2 image"), 1, {
-        bezier: {
-            values: [{ y: "+=20" }, { y: "-=10" }, { y: "+=15" }, { y: "-=10" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-    cloudsTimeLine2.set($(".cloud-size-2 image"), {
-        x: "+=3200",
-    });
-    cloudsTimeLine2.to($(".cloud-size-2 image"), 1, {
-        bezier: {
-            values: [{ y: "-=20" }, { y: "+=10" }, { y: "-=15" }, { y: "+=10" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-
-
-
-    let cloudsTimeLine3 = new TimelineMax({ repeat: -1 });
-
-    cloudsTimeLine3.to($(".cloud-size-3 image"), 1, {
-        bezier: {
-            values: [{ y: "+=20" }, { y: "-=10" }, { y: "+=15" }, { y: "-=10" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-    cloudsTimeLine3.set($(".cloud-size-3 image"), {
-        x: "+=3200",
-    });
-    cloudsTimeLine3.to($(".cloud-size-3 image"), 1, {
-        bezier: {
-            values: [{ y: "-=20" }, { y: "+=10" }, { y: "-=15" }, { y: "+=10" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-
-
-
-    let cloudsTimeLine4 = new TimelineMax({ repeat: -1 });
-
-    cloudsTimeLine4.to($(".cloud-size-4 image"), 1, {
-        bezier: {
-            values: [{ y: "+=20" }, { y: "-=10" }, { y: "+=15" }, { y: "-=10" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-    cloudsTimeLine4.set($(".cloud-size-4 image"), {
-        x: "+=3200",
-    });
-    cloudsTimeLine4.to($(".cloud-size-4 image"), 1, {
-        bezier: {
-            values: [{ y: "-=20" }, { y: "+=10" }, { y: "-=15" }, { y: "+=10" }],
-
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-
-
-
-
-
-    let cloudsTimeLine5 = new TimelineMax({ repeat: -1 });
-
-    cloudsTimeLine5.to($(".cloud-size-5 image"), 1, {
-        bezier: {
-            values: [{ y: "+=20" }, { y: "-=10" }, { y: "+=15" }, { y: "-=10" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-    cloudsTimeLine5.set($(".cloud-size-5 image"), {
-        x: "+=3200",
-    });
-    cloudsTimeLine5.to($(".cloud-size-5 image"), 1, {
-        bezier: {
-            values: [{ y: "-=20" }, { y: "+=10" }, { y: "-=15" }, { y: "+=10" }],
-        },
-        x: "-=1600",
-        ease: Power0.easeNone,
-    });
-
-    cloudsTimeLine1.timeScale(1);
-    cloudsTimeLine2.timeScale(0.9);
-    cloudsTimeLine3.timeScale(0.8);
-    cloudsTimeLine4.timeScale(0.7);
-    cloudsTimeLine5.timeScale(0.6);
-
-    let timelines = [cloudsTimeLine1, cloudsTimeLine2, cloudsTimeLine3, cloudsTimeLine4, cloudsTimeLine5];
-    timelines.forEach(e => { e.timeScale(e.timeScale() * 0.3); });
-
-}
-
-let svg = { width: 1365, height: 650 };
-
 
 function makeCloudsTimeLine() {
 
@@ -139,61 +15,134 @@ function makeCloudsTimeLine() {
     });
 
     tl.add("cloud-action");
-    let randomBezier = () => new Object({ values: Array(4).fill(0).map((v, i) => new Object({ y: `${(i % 2 == 0 ? "+=" : "-=")}${Math.random() * 20}` })) });
-    let xLocation = (i, e) => $(e).position().left + 200 < svg.width / 2 ? "-=200" : "+=200";
-    let transformOriginFunc = (i, e) => $(e).position().left + 200 < svg.width / 2 ? "100% 20%" : "0 20%";
-    let clouds = [
-        { class: ".cloud-size-1", time: 4.6 },
-        { class: ".cloud-size-2", time: 4.2 },
-        { class: ".cloud-size-3", time: 3.8 },
-        { class: ".cloud-size-4", time: 3.4 },
-        { class: ".cloud-size-5", time: 3 },
-    ];
-    tl.to($(".window"), 2, {
+    let randomBezier = () => new Object({ values: Array(3).fill(0).map((v, i) => new Object({ y: `${(i % 2 == 0 ? "+=" : "-=")}${Math.random() * 20}` })) });
+    let xLocation = (i, e) => {
+        let { left } = $(e).position();
+        left = Math.round(left * 100 / $(window).width()); // percentage left 
+        console.log(left);
+        return left < 50 ? "-=70%" : "+=70%";
+    };
+    
+    /*tl.to($(".window"), 2, {
         scale: 2,
         transformOrigin: "90% center",
         opacity: 0,
 
         ease: Power3.easeInOut,
 
-    }, "cloud-action");
-    clouds.forEach(cloud => {
-
-        tl.to($(`${cloud.class} image`), cloud.time, {
-            bezier: randomBezier,
-            scale: "+=2",
-            transformOrigin: transformOriginFunc,
-            ease: Power3.easeIn,
-            x: xLocation,
-            opacity: 0
-        }, "cloud-action");
-
-    });
-
-
-    tl.to($(".airport-background"), 8, {
-        scale: 3,
-        transformOrigin: "center center",
+    }, "cloud-action");*/
+    tl.to("#clouds-viewport" , 7 , {
+        perspective : 250 , 
         ease: Power2.easeInOut
-    }, "cloud-action");
+    } , 1 ); 
+
+    return tl;
+
+}
 
 
-    return tl; 
+function XYtoTopLeftPercent({ x, y }) {
+    return { left: `${x}%`, top: `${y}%` };
+}
+
+function XYtoSecreenPercent({ x, y }) {
+
+    let width = $('html').width();
+    let height = $('html').height();
+    x = Math.round(x * 1000 / width) / 10;
+    y = Math.round(y * 1000 / height) / 10;
+    return { x, y };
+}
+
+
+// function quadraticLine(start, center, end) {
+//     let curve = "M" + start.x + " " + start.y + " Q " + center.x + " " + center.y + " " + end.x + " " + end.y;
+//     return makeSVG("path", {
+//         d: curve, "stroke-width": 7, class: 'trip-line', stroke: '#f00',
+//     });
+
+// }
+// function drawQuadratic(start, center, end) {
+//     $(".test-svg").append(quadraticLine(start, center, end));
+// }
+
+class Airplane {
+
+
+
+    getPointAtPercent(percent) {
+        percent = percent * this.pathLength / 100;
+        let point = this.path.getPointAtLength(percent);
+        let matrix = this.path.getScreenCTM();
+        let position = point.matrixTransform(matrix);
+        return position;
+    }
+
+    getPointAtScreen(percent) {
+        let point = this.getPointAtPercent(percent);
+
+        return XYtoTopLeftPercent(XYtoSecreenPercent(point));
+    }
+
+    constructor() {
+        let curve = "M1077.9,128.9C1054.1,271.1,927.8,361,883.1,361c-415.7,0-534.4,7.8-534.4,7.8c-50.9,0-195.6,102.7-195.6,224.3";
+        //    this.path = makeSVG("path", { d: curve });
+        this.path = document.getElementById("airplane-path");
+        this.pathLength = this.path.getTotalLength();
+        this.airplane = $("#airplane");
+    }
+
+    async makeTimeLine() {
+
+        let obj = { position: 0 };
+
+        let tl = new TimelineMax({
+        });
+
+
+        let values = await new Promise((res, rej) => {
+
+
+            let result = [];
+            let frames = 5;
+
+            for (let i = 0; i <= frames; i++) {
+                let precent = i * 100 / frames;
+                let p = this.getPointAtScreen(100 - precent);
+                result.push(p);
+            }
+            res(result);
+        });
+
+        tl.set(this.airplane, { ...values[0] });
+        tl.to(this.airplane, 4, {
+            bezier: {
+                values,
+                autoRotate: ["left", "top", "rotation", 90, false]
+            },
+        });
+
+    }
 
 }
 $(function () {
 
-
-
-
-    makeCloudsTimeLine().play(1000);
+  // let cloudsTimeLine =  makeCloudsTimeLine(); 
+    let airplane = new Airplane();
+    //airplane.makeTimeLine();
 
     let controller = new ScrollMagic.Controller();
 
     /*
-    var scene = new ScrollMagic.Scene({ offset: 0, duration: 2000 })
+    var scene = new ScrollMagic.Scene({ offset: 432, duration: 2000 })
         .setPin("#header")
-        .addTo(controller);*/
+        .setTween(cloudsTimeLine)
+        .addTo(controller);
+*/
 
 
 });
+
+
+
+
