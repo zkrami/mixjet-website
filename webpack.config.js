b17056bb1345094966e2d49c6357149937ea9dc0
Rami Al-Zebak <17zrami@gmail.com>
2019-09-22 17:27:25 +0300

    parallax effect

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,40 @@
 var webpack = require('webpack');
-var path = require("path"); 
+var path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = true ; 
 module.exports = {
     entry: {
-        entry: __dirname + '/entry.js' , 
-        test: __dirname + '/test.js', 
-        canvas: __dirname + '/canvas.js', 
+        entry: __dirname + '/entry.js',
+        test: __dirname + '/test.js',
+        canvas: __dirname + '/canvas.js',
+        style: __dirname + '/assets/sass/test.scss'
     },
     output: {
         path: path.resolve(__dirname, 'assets'),
-        filename: 'js/[name].bundle.js' , 
+        filename: 'js/[name].bundle.js',
     },
     module: {
-
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: true
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
 
+        })
+    ]
 }
